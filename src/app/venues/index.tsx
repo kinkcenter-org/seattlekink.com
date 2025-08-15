5ed589172172e79adf77cde562b16531a81976b4
Raven Dubh <tehraven@gmail.com>
2025-08-14 19:45:09 -0700

    Fix export

--- src/app/venues/index.tsx
+++ src/app/venues/index.tsx
@@ -1,5 +1,6 @@
 import { Venue } from './types'
 
+import LinkButton from '@/src/components/linkButton'
 import galleryErato from './gallery-erato'
 import kinkcenter from './kinkcenter'
 import subspace from './subspace'
@@ -12,6 +13,31 @@ export const VenueList: Venue[] = [
     kinkcenter,
 ]
 
-export const makeVenuePage = ({ name }: Venue) => {
-    return <p>{name}</p>
-}
+export const VenueComponent = ({
+    name,
+    description,
+    comments,
+    website,
+}: Venue) => (
+    <div
+        key={name}
+        className="flex flex-col gap-4 items-start place-items-start text-start border-l-2 pl-5 w-full"
+    >
+        <p className="text-xl font-bold">{name}</p>
+        <div className="relative left-8">
+            <blockquote className="italic flex flex-col gap-2">
+                {Array.from([description])
+                    .flat()
+                    .map((desc, i) => (
+                        <p key={`${name}-${i}`}>{desc}</p>
+                    ))}
+            </blockquote>
+        </div>
+        {comments
+            ? Array.from([comments])
+                  .flat()
+                  .map((desc, i) => <p key={`${name}-${i}`}>{desc}</p>)
+            : null}
+        {website ? <LinkButton href={website}>Website</LinkButton> : null}
+    </div>
+)
--- src/app/venues/page.tsx
+++ src/app/venues/page.tsx
@@ -1,37 +1,7 @@
-import LinkButton from '@/src/components/linkButton'
-import { VenueList } from '.'
-import { Venue } from './types'
+import { VenueComponent, VenueList } from '.'
 
 import './venue.css'
 
-export const VenueComponent = ({
-    name,
-    description,
-    comments,
-    website,
-}: Venue) => (
-    <div
-        key={name}
-        className="flex flex-col gap-4 items-start place-items-start text-start border-l-2 pl-5 w-full"
-    >
-        <p className="text-xl font-bold">{name}</p>
-        <div className="relative left-8">
-            <blockquote className="italic flex flex-col gap-2">
-                {Array.from([description])
-                    .flat()
-                    .map((desc, i) => (
-                        <p key={`${name}-${i}`}>{desc}</p>
-                    ))}
-            </blockquote>
-        </div>
-        {comments
-            ? Array.from([comments])
-                  .flat()
-                  .map((desc, i) => <p key={`${name}-${i}`}>{desc}</p>)
-            : null}
-        {website ? <LinkButton href={website}>Website</LinkButton> : null}
-    </div>
-)
 const Venues = () => (
     <main className="flex flex-col gap-[32px] max-w-[800] items-center place-items-center text-center">
         <p className="text-2xl">Venues</p>
