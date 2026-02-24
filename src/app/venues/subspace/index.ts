50ce25ab5e3405737bf430556b4b8965956babef
Raven Dubh <rdubh@xembly.com>
2026-02-24 13:13:54 -0100

    Update subspace

--- src/app/venues/subspace/index.ts
+++ src/app/venues/subspace/index.ts
@@ -6,8 +6,8 @@ export default {
   description: ["We are a dungeon/studio play space in South Seattle"],
   comments: [
     "SubSpace hosts routine workshops, socials, and play parties.",
-    "This venue is in an industrial complex in Tukwila and is not easy to get to if you don't drive",
-    "SubSpace is mostly rented by entities running events on their own",
+    "This venue is in an industrial complex in Tukwila",
+    "SubSpace is mostly an event venue, hosting a fair amount of events and classes",
   ],
   image,
   socials: [
