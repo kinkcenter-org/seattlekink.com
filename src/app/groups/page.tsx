dbff9024e6eaeeb2e334667aa1c4b22817b8b71e
Raven Dubh <rdubh@xembly.com>
2026-02-24 13:09:41 -0100

    Update group language

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -5,11 +5,14 @@ import "../venues/venue.css";
 const Groups = () => (
   <main className="flex flex-col gap-8 items-center place-items-center text-center">
     <p className="text-xl border-b pb-2 w-full">Groups</p>
-    <p className="text-lg mb-4">
+    <p className="text-lg mb-2">
       Groups are usually just a set of people who organize and run events
     </p>
-    <p className="text-lg mb-4">
+    <p className="text-lg mb-2">
       You'll usually see Socials, Munches, and online meetings from them
+    </p>
+    <p className="text-lg mb-2">
+      Some groups are only communities that do not run their own events
     </p>
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 w-full">
       {GroupList.map(VenueComponent)}
