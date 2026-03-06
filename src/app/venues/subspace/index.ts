f913610293f9fe52adf9bf877f92509c8f1c6b04
Raven Dubh <rdubh@xembly.com>
2026-03-06 15:15:48 -0800

    Udpate SS

--- src/app/venues/subspace/index.ts
+++ src/app/venues/subspace/index.ts
@@ -5,12 +5,11 @@ export default {
   name: "SubSpace",
   description: [
     "We are a dungeon/studio play space in South Seattle",
-    "We are open for private rentals for small groups of kinksters, feel free to message us for details on how to book the play space for your event or private play time",
     "We are available for private rentals and event rentals",
+    "We are a non-discriminatory, all-inclusive membership organization. Whatever your kink or orientation is, you are welcome at SubSpace. We host about 200 events/year",
   ],
   comments: [
     "SubSpace hosts routine workshops, socials, and play parties at their venue in Tukwila",
-    "SubSpace is a non-discriminatory, all-inclusive membership organization. Whatever your kink or orientation is, you are welcome at SubSpace. We host about 200 events/year",
   ],
   image,
   socials: [
